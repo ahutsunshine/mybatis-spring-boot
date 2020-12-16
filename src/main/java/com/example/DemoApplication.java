8104e94eb47d5db79e5aa395e92aed266be7148e
deyiyao <deyiyao@ebay.com>
2020-12-17 00:21:09 +0800

    add corp for apis

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,12 +1,19 @@
 package com.example;
 
+import com.example.common.SimpleCORSFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-	  SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public SimpleCORSFilter simpleFilter() {
+        return new SimpleCORSFilter();
+    }
 }
